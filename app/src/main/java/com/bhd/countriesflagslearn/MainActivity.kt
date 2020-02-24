ac5afcfdd54dc59c5e9df7814d842608c9b8c60b
BHD233 <32787292+BHD233@users.noreply.github.com>
2020-02-24 18:19:25 +0700

    Add require permission
    New:
     + App will ask for permission automactically
    To do:
     - Leading board

--- app/src/main/java/com/bhd/countriesflagslearn/MainActivity.kt
+++ app/src/main/java/com/bhd/countriesflagslearn/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.bhd.countriesflagslearn
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import okhttp3.Call
@@ -16,6 +20,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //check permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
+                101)
+        }
     }
 
     fun onLearnButtonClicked(view: View){
